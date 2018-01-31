f26a57aac213db7a1a2ef2bf4f415d8954aadb16
Anastasia Stieb <anastasia.stieb@xing.com>
2018-01-31 10:48:21 +0100

    course ready

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript. The Good Parts', pages: 105 },
+    { title: 'Harry Potter', pages: 41 },
+    { title: 'Anna Karenina', pages: 235 },
+    { title: 'Idiot', pages: 5 }
+  ];
+}
